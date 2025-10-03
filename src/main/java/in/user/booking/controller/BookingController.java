f85c423cea97d88437b21705f736dd76dff2bdb9
adinathbiradar <adinathbiradar77@gmail.com>
2025-10-03 16:41:52 +0530

    updated

--- src/main/java/in/user/booking/controller/BookingController.java
+++ src/main/java/in/user/booking/controller/BookingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bookings")
+@RequestMapping("/api/bookings")
 @CrossOrigin(origins = "http://localhost:4200")
 public class BookingController {
 
